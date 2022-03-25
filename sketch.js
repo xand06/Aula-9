54022a8e6816d51725007a4619c3891c80705037
xand06 <102333860+xand06@users.noreply.github.com>
2022-03-24 21:03:57 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+var quadrado;
+function setup() {
+  createCanvas(400,400);
+quadrado = createSprite(200,200,30,30)
+
+
+
+
+}
+
+function draw() 
+{
+  background(30);
+if (keyIsDown (DOWN_ARROW)) {
+  quadrado.position.y += 5
+}
+
+
+
+
+
+
+
+drawSprites()
+}
+
+
+
+
